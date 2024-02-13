d7dc7c35cf383a9a9096afa7b5f21d9f344be243
velvet <159878397+chin6112@users.noreply.github.com>
2024-02-13 16:44:20 +0700

    Add files via upload

---
+++ app02.py
@@ -0,0 +1,9 @@
+# streamlit run streamlit/app02.py
+import streamlit as st
+st.sidebar.text('Sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.title('OOP in ML')
+
+st.text_input("what's your name?")
